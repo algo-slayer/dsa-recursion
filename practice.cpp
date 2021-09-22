fd317b43ec470646a4a8436fec1c1551efaa83b8
arunava2331 <arunavasaha2331@gmail.com>
2021-09-22 19:05:42 +0530

    Add practice

---
+++ practice.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ll x;
+    cin >> x;
+
+    cout << x << endl;
+    return 0;
+}
